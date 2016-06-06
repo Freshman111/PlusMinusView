ed15ef8b335cf394212cfbac0971f143c14c78af
liumengkai <sgffsg@126.com>
2016-06-06 12:02:45 +0800

    add plusminus view

--- app/src/main/java/com/sgffsg/plusminusview/MainActivity.java
+++ app/src/main/java/com/sgffsg/plusminusview/MainActivity.java
@@ -2,12 +2,44 @@ package com.sgffsg.plusminusview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.sgffsg.plusminusview.view.PlusMinusView;
 
 public class MainActivity extends AppCompatActivity {
+
+    PlusMinusView plusMinusView;
+    TextView tvSum;
+    Button btnBuy;
+    private float price=8.5f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plusMinusView= (PlusMinusView) findViewById(R.id.item_plusminusview);
+        tvSum= (TextView) findViewById(R.id.tv_sum_num);
+        btnBuy= (Button) findViewById(R.id.btn_buy);
+        plusMinusView.setMaxNum(30);
+        plusMinusView.setPlusMinusListener(new PlusMinusView.OnPlusMinusClickListener() {
+            @Override
+            public void onPlusClick(int currentNum) {
+                showSum(currentNum);
+            }
+
+            @Override
+            public void onMinusClick(int currentNum) {
+                showSum(currentNum);
+            }
+        });
+
+
+
+    }
+
+
+    private void showSum(int num) {
+        tvSum.setText(price*num+"$");
     }
 }
